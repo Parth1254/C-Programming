d087c20cf26fb3651576544288c9c6e35bf6ac45
Parth1254 <161573891+Parth1254@users.noreply.github.com>
2024-03-18 06:50:27 +0530

    Update 7.swap.c

--- 7.swap.c
+++ 7.swap.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+void swap(double *a, double *b) {
+    double temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    double first, second;
+    printf("Enter first number: ");
+    scanf("%lf", &first);
+    printf("Enter second number: ");
+    scanf("%lf", &second);
+
+    swap(&first, &second);
+
+    printf("\nAfter swapping, first number = %.2lf\n", first);
+    printf("After swapping, second number = %.2lf", second);
+
+    return 0;
+}
 
